1e8541944b6df4e9db5e59391cebc14ec1b2ee06
Charlotte Marion <marion.charlotte@orange.fr>
2022-04-09 11:59:04 +0200

    exercice 1

--- src/app/exercice1/exercice1.component.ts
+++ src/app/exercice1/exercice1.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exercice1.component.css']
 })
 export class Exercice1Component implements OnInit {
+  public messages: Message[] = [
+    {
+      title: "Bonjour",
+    }
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public inputType = "date"
+
+  public onClick(event: Event): void {
+    console.log("bonjour");
   }
 
 }
